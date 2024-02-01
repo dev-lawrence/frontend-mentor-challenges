3575de7f442d79dc3a5e607c817313ef53b2ed60
dev-lawrence <lawrenceamrasakpare19@gmail.com>
2024-02-01 10:57:11 +0100

    Remove some files and also addes some files

--- results-summary-component-main/src/App.jsx
+++ results-summary-component-main/src/App.jsx
@@ -1,7 +1,52 @@
+import { data } from './data.js';
+
 const App = () => {
   return (
     <>
-      <h1>Yo</h1>
+      <div className="app">
+        <section className="container">
+          <div className="d-grid">
+            <div className="result">
+              <h1>Your Result</h1>
+              <div>
+                <div className="score">
+                  <p>76</p>
+                  <span>of 100</span>
+                </div>
+              </div>
+
+              <div className="text">
+                <h2>Great</h2>
+                <p>
+                  You Scored higher than 65% of the people who have taken the
+                  test
+                </p>
+              </div>
+            </div>
+            <div className="summary">
+              <h3>Summary</h3>
+              {data.map((item, index) => (
+                <div key={index} className="entry">
+                  <div className="flex">
+                    <div className="stat">
+                      <img src={item.icon} alt="" />
+                      <h4>{item.category}</h4>
+                    </div>
+
+                    <div className="score">
+                      <p>{item.score}/100</p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+
+              <p></p>
+
+              <button>Continue</button>
+            </div>
+          </div>
+        </section>
+      </div>
     </>
   );
 };
--- results-summary-component-main/src/data.js
+++ results-summary-component-main/src/data.js
@@ -1,22 +1,27 @@
+import Reaction from './assets/icon-reaction.svg';
+import Memory from './assets/icon-memory.svg';
+import Verbal from './assets/icon-verbal.svg';
+import Visual from './assets/icon-visual.svg';
+
 export const data = [
   {
     category: 'Reaction',
     score: 80,
-    icon: './icon-reaction.svg',
+    icon: Reaction,
   },
   {
     category: 'Memory',
     score: 92,
-    icon: './assets/icon-memory.svg',
+    icon: Memory,
   },
   {
     category: 'Verbal',
     score: 61,
-    icon: './assets/icon-verbal.svg',
+    icon: Verbal,
   },
   {
     category: 'Visual',
     score: 72,
-    icon: './assets/icon-visual.svg',
+    icon: Visual,
   },
 ];
